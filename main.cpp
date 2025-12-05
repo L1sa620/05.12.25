dfe8000a90a23853884704110209ce5d98b90bf2
L1sa620 <lisevbom@gmail.com>
2025-12-05 23:31:21 +0300

    big fix and writing functions

--- main.cpp
+++ main.cpp
@@ -1,96 +1,130 @@
 #include <iostream>
-namespace topit{
-  struct p_t {
+#include <ostream>
+#include <stdexcept>
+
+namespace topit
+{
+  struct p_t
+  {
     int x, y;
   };
   bool operator==(p_t, p_t);
   bool operator!=(p_t, p_t);
-  struct f_t {
+  struct f_t
+  {
     p_t aa, bb;
   };
-  struct IDraw{
+  struct IDraw
+  {
     virtual ~IDraw() = default;
     virtual p_t begin() const = 0;
     virtual p_t next(p_t) const = 0;
   };
-  struct Dot: IDraw{
+  struct Dot: IDraw
+  {
     Dot(int x, int y);
-    explicit Dot(p_t dd)
+    explicit Dot(p_t dd);
     p_t begin() const override;
     p_t next(p_t) const override;
     p_t d;
-    size_t points(const IDraw& d, p_t** pts, size_t s);
-    f_t frame(const p_t* pts, size_t s);
-    char * canvas(f_t fr, chat fill);
-    void paint(char * cnv, f_t fr, p_t p, char fill);
-    void fluch(std::ostream& os, const char *, f_t ft);
   };
+  size_t points(const IDraw& d, p_t** pts, size_t s);
+  f_t frame(const p_t* pts, size_t s);
+  char* canvas(f_t fr, char fill);
+  void paint(char* cnv, f_t fr, p_t p, char fill);
+  void flush(std::ostream& os, const char* cnv, f_t fr);
 }
 
 int main()
 {
-  using topit::IDraw;
-  using topit::Dot;
-  using topit::f_t;
-  using topit::p_t;
+  using namespace topit;
   int err = 0;
-  p_t * pts = nullptr;
   IDraw* shps[3] = {};
-  try {
-    shps[0] = new Dot(0,0);
-    shps[0] = new Dot(5,7);
-    shps[0] = new Dot(-5,-2);
-  for (size_t i = 0; i < 3; ++i)
+  p_t* pts = nullptr;
+  size_t s = 0;
+  try
   {
-    s += points(*(shps[i]), &pts, s);
+    shps[0] = new Dot(0, 0);
+    shps[1] = new Dot(5, 7);
+    shps[2] = new Dot(-5, 2);
+    for (size_t i = 0; i < 3; ++i)
+    {
+      s += points(*(shps[i]), &pts, s);
+    }
+    f_t fr = frame(pts, s);
+    char* cnv = canvas(fr, '.');
+    for (size_t i = 0; i < s; ++i)
+    {
+      paint(cnv, fr, pts[i], '#');
+    }
+    flush(std::cout, cnv, fr);
+    delete[] cnv;
   }
-  f_t fr = frame(pts, s);
-  char * cnv = canvas(fr, '.');
-  fot (size_t i = 0; i < s; ++i)
+  catch (...)
   {
-  paint(cnv, ft, pts[i], '#');
-  }
-  flush(std::cout, cnv, ft);
-  delete [] cnv;
- } catch(...) {
     err = 2;
     std::cerr << "Bad drawing\n";
   }
-  delete [] pts;
+
   delete shps[0];
   delete shps[1];
   delete shps[2];
+  delete[] pts;
   return err;
 }
-  topit::Dot::Dot(p_t dd):
-    IDraw(),
-    d{dd}
-    {}
 
-  topit::Dot::Dot(int x, int y):
-    IDraw(),
-    d{x,y}
-    {}
+topit::Dot::Dot(p_t dd):
+  IDraw(),
+  d{dd}
+{}
+topit::Dot::Dot(int x, int y):
+  IDraw(),
+  d{x, y}
+{}
+topit::p_t topit::Dot::begin() const
+{
+  return d;
+}
+topit::p_t topit::Dot::next(p_t prev) const
+{
+  if (prev != begin())
+  {
+    throw std::logic_error("bad impl");
+  }
+  return d;
+}
+bool topit::operator==(p_t a, p_t b)
+{
+  return a.x == b.x && a.y == b.y;
+}
+bool topit::operator!=(p_t a, p_t b)
+{
+  return !(a == b);
+}
+size_t topit::points(const IDraw& d, p_t** pts, size_t s)
+{
+  // TODO
+  return 0;
+}
 
-  topit::p_t topit::Dot::begin() const {
-    return d;
-  }
-  topit::p_t topit::Dot::next(p_t prev) const {
-    if (prev != begin())
-    {
-      throw std::logic_error("bad impl");
-    }
-    return d;
-  }
-  bool topit::operator==(p_t a, p_t b){
-    return a.x == b.x && a.y == b.y;
-  }
-  bool topit::operator!=(p_t a, p_t b){
-    return !(a == b);
-  }
+topit::f_t topit::frame(const p_t* pts, size_t s)
+{
+  // TODO
+  return f_t{{0, 0}, {0, 0}};
+}
 
-    //IDraw * p = new Dot(10, 10);
-  //IDraw * pp = new Dot(p_t(10, 10);
+char* topit::canvas(f_t fr, char fill)
+{
+  // TODO
+  return nullptr;
+}
 
-  //p_t a{1,0}, b{1,0};
-  //std::cout << (a != b) << "\n";
+void topit::paint(char* cnv, f_t fr, p_t p, char fill)
+{
+  // TODO
+}
+
+void topit::flush(std::ostream& os, const char* cnv, f_t fr)
+{
+  // TODO
+}
